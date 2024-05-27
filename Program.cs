6adac00bb6d3190e45803b44ad829325756cc0fa
Felipe Mendes <you@example.com>
2024-05-26 23:44:03 -0300

    Exercircio Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("---soletrando---");
+Console.WriteLine("---digite c para começar a contagem regressiva---");
+
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("---iniciando contagem regressiva...---");
+
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.Write(" Preparar...");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.Write(" Apontar...");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.Write(" FOGO!!!");
